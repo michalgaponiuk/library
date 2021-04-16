928caef6235431ae96e297a3dfd868f7777e2d57
michalgaponiuk <michal.gaponiuk@gmail.com>
2021-04-16 23:40:39 +0200

    added price

--- src/Book.java
+++ src/Book.java
@@ -1,11 +1,13 @@
 public class Book {
     String name;
+    int price;
 
-    Book(String name) {
+    Book(String name, int price) {
         this.name = name;
+        this.price = price;
     }
 
     public static void main(String[] args) {
-        Book book = new Book("Kubus");
+        Book book = new Book("Kubus",100);
     }
 }
